7c7b9a6af1f8026138281789d535b2741df978f8
snavti <navti@hotmail.com>
2016-10-25 16:44:39 +0100

    Update pg_backup.sh

--- pg_backup.sh
+++ pg_backup.sh
@@ -7,5 +7,5 @@ RETENTION=3
 DATE=`date +%Y%m%d_%H%M`
 
 find /var/lib/postgresql/ -name "*.sql.bz2" -mtime +$RETENTION -exec rm {} \;
-pg_dump $DATABASE -U $USER > /var/lib/postgres/$DATABASE-dump-$DATE.sql
+pg_dump $DATABASE -U $USER > /var/lib/postgresql/$DATABASE-dump-$DATE.sql
 
