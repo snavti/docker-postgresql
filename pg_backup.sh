7afe236673c107403f6075a08d3a894f05bc01d0
snavti <navti@hotmail.com>
2016-10-25 12:51:14 +0100

    Update pg_backup.sh

--- pg_backup.sh
+++ pg_backup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 export PGPASSWORD=YourPassword
-DATABASE=YourDatabase
-USER=webadmin
+DATABASE=YourDatabaseName
+USER=YourDatabaseUser
 RETENTION=3
 DATE=`date +%Y%m%d_%H%M`
 
 find /var/lib/postgres/backup/ -name "*.sql.bz2" -mtime +$RETENTION -exec rm {} \;
 pg_dump $DATABASE -U $USER > /var/lib/postgres/backup/$DATABASE-dump-$DATE.sql
+
